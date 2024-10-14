f4d77b10ece1df592b5a9245ab448aa904d8fdb8
Pinpas <hugo.fransen03@gmail.com>
2024-10-14 23:16:13 +0200

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5D1SZN52NB"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5D1SZN52NB');
+            `,
+          }}
+        />
+      </head>
       <body className={nunito.className}>
         {children}
         <Script
